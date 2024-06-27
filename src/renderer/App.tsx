0c9b01358e9eaf64848f439abb4c9b5d9cd2264b
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2024-06-27 10:34:54 +0200

    Changing login

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,24 +5,19 @@ import { CommandProvider } from './contexts/CommandContext';
 import Background from './components/Background';
 import AppBar from './components/AppBar';
 import { ThemeProvider } from './contexts/ThemeContext';
-import SignUp from './components/SignUp';
-import { UserProvider } from './contexts/UserContext';
 
 export default function App() {
   return (
-    <UserProvider>
       <ThemeProvider>
         <CommandProvider>
           <AppBar />
           <Background />
           <Router>
             <Routes>
-              <Route path="/" element={<SignUp />} />
-              <Route path="/ui" element={<UI />} />
+              <Route path="/" element={<UI />} />
             </Routes>
           </Router>
         </CommandProvider>
       </ThemeProvider>
-    </UserProvider>
   );
 }
